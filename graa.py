1de65a9067ec16bb024533a31c8d423d8c28d8d5
aiwinska <a.iwinska@student.uw.edu.pl>
2026-07-04 15:08:55 +0200

    zmiana

--- graa.py
+++ graa.py
@@ -27,3 +27,65 @@ COLOR_GREEN = (65, 165, 100)
 COLOR_RED = (190, 65, 65)
 COLOR_ORANGE = (210, 130, 50)
 COLOR_PURPLE = (150, 100, 200)
+
+def fallback_oxygen():
+    surf = pygame.Surface((30, 30), pygame.SRCALPHA)
+    pygame.draw.circle(surf, (70, 160, 220), (15, 15), 10)
+    return surf
+
+
+def fallback_glucose():
+    surf = pygame.Surface((30, 30), pygame.SRCALPHA)
+    pygame.draw.polygon(surf, (90, 175, 90), [(15, 2), (28, 10), (28, 22), (15, 29), (2, 22), (2, 10)])
+    return surf
+
+
+def fallback_amino():
+    surf = pygame.Surface((30, 30), pygame.SRCALPHA)
+    pygame.draw.polygon(surf, (200, 100, 140), [(15, 3), (27, 26), (3, 26)])
+    return surf
+
+
+def fallback_toxin():
+    surf = pygame.Surface((35, 35), pygame.SRCALPHA)
+    points = [(17, 2), (21, 14), (32, 14), (23, 22), (27, 33), (17, 26), (7, 33), (11, 22), (2, 14), (13, 14)]
+    pygame.draw.polygon(surf, COLOR_RED, points)
+    return surf
+
+
+def fallback_receptor():
+    surf = pygame.Surface((50, 50), pygame.SRCALPHA)
+    pygame.draw.circle(surf, (140, 175, 200), (25, 25), 24, 2)
+    pygame.draw.circle(surf, (100, 130, 160), (25, 25), 8)
+    return surf
+
+
+def fallback_stem():
+    surf = pygame.Surface((40, 40), pygame.SRCALPHA)
+    pygame.draw.circle(surf, (100, 200, 255), (20, 20), 16)
+    pygame.draw.circle(surf, (255, 255, 255), (20, 20), 6)
+    return surf
+
+
+def fallback_cancer():
+    surf = pygame.Surface((40, 40), pygame.SRCALPHA)
+    pygame.draw.circle(surf, (130, 40, 40), (20, 20), 18)
+    pygame.draw.circle(surf, (50, 10, 10), (20, 20), 8)
+    return surf
+
+
+def fallback_tissue(color):
+    surf = pygame.Surface((40, 40), pygame.SRCALPHA)
+    pygame.draw.rect(surf, color, (4, 4, 32, 32), border_radius=6)
+    pygame.draw.rect(surf, (200, 200, 200), (4, 4, 32, 32), 1, border_radius=6)
+    return surf
+
+
+def load_image(filename, size, fallback_func):
+    if os.path.exists(filename):
+        try:
+            img = pygame.image.load(filename).convert_alpha()
+            return pygame.transform.scale(img, size)
+        except:
+            return fallback_func()
+    return fallback_func()
